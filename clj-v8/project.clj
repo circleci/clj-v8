5966f81b5fd63b6db257bc7efa602cfa8aab6724
Paul Biggar <paul.biggar@gmail.com>
2012-05-12 18:29:21 -0700

    Update native-dependencies

--- clj-v8/project.clj
+++ clj-v8/project.clj
@@ -1,4 +1,6 @@
 (defproject clj-v8 "0.1.0"
   :description "A Clojure wrapper for the v8 Javascript library"
   :dependencies [[org.clojars.elmom/clojure-jna "0.9"]]
-  :dev-dependencies [[midje "1.3.1"]])
+  :native-dependencies [[clj-v8-native/clj-v8-native "0.1.0"]]
+  :dev-dependencies [[midje "1.3.1"]
+                     [native-deps "1.0.5"]])
