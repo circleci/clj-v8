a0cead4d1f98329110f27ab041c8269c90b752e8
Paul Biggar <paul.biggar@gmail.com>
2012-05-13 17:09:18 -0700

    Use lein's built-in dev-dependencies

--- clj-v8/project.clj
+++ clj-v8/project.clj
@@ -1,7 +1,6 @@
 (defproject clj-v8 "0.1.0"
   :description "A Clojure wrapper for the v8 Javascript library"
-  :dependencies [[org.clojars.elmom/clojure-jna "0.9"]]
-  :native-dependencies [[clj-v8-native "0.1.0"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [redd/native-deps "1.0.7"]]
-  :jvm-opts ["-Djna.library.path=native/macosx/x86_64"])
+  :dependencies [[org.clojars.elmom/clojure-jna "0.9"]
+                 [clj-v8-native/clj-v8-native "0.1.1"]]
+  :dev-dependencies [[midje "1.3.1"]]
+  :jvm-opts ["-Djna.library.path=native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"])
