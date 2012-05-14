e54b0677e4eee5325528afb5bcde87c99d0325d2
Paul Biggar <paul.biggar@gmail.com>
2012-05-13 18:19:49 -0700

    Add more tests

--- clj-v8/test/v8/test_core.clj
+++ clj-v8/test/v8/test_core.clj
@@ -4,3 +4,9 @@
 
 (fact "run-script works"
   (v8/run-script "3/0") => "Infinity")
+
+(fact "complex code works"
+  (v8/run-script "(function(){ return 5; })();") => "5")
+
+(fact "syntax error doesnt die"
+  (v8/run-script "sjd2-23=dfv;2-") => "")
