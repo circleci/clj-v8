0269df3c7231a5c176c1283d7b2c491aaed0f7ee
Ivan Stojic <ivans@ordecon.com>
2012-06-24 16:09:40 +0200

    extract conversion of return value from c++ to jna, error/exception handling, new osx binaries

--- clj-v8-native/src/v8wrapper/v8wrapper.cc
+++ clj-v8-native/src/v8wrapper/v8wrapper.cc
@@ -1,18 +1,31 @@
 #include <cstdlib>
+#include <iostream>
 #include <wchar.h>
-
+#include <errno.h>
 #include <v8.h>
 #include <string.h>
-
 #include "v8wrapper.h"
 
 using namespace v8;
 
-const wchar_t *run(const wchar_t *jssrc)
-{
-  TryCatch try_catch;
-  HandleScope handle_scope;
+wchar_t *fromValue(Handle<Value> v) {
+  String::Value ucs2str(v);
+  
+  wchar_t *result = (wchar_t*) calloc(sizeof(wchar_t), ucs2str.length() + 1);
+  
+  wchar_t *dst = result;
+  uint16_t *src = *ucs2str;
 
+  while (*src) {
+    *dst++ = *src++;
+  }
+  
+  return result;
+}
+
+
+wchar_t *run(wchar_t *jssrc) {
+  HandleScope handle_scope;
   Persistent<Context> context = Context::New();
   Context::Scope context_scope(context);
 
@@ -23,26 +36,41 @@ const wchar_t *run(const wchar_t *jssrc)
     uint16_t *dst = jsu16;
     const wchar_t *src = jssrc;
 
-    while (*dst++ = *src++) {}
+    int count = 0;
+    
+    while (*src) {
+      *dst++ = *src++;
+      count++;
+    }
   }
 
+  
   Handle<Script> script = Script::Compile(String::New(jsu16));
 
   if (*script != NULL) {
+    TryCatch trycatch;
+    
     Handle<Value> v = script->Run();
 
-    String::Value ucs2str(v);
-    wchar_t *result = (wchar_t*) calloc(sizeof(wchar_t), ucs2str.length() + 1);
-    wchar_t *dst = result;
-    uint16_t *src = *ucs2str;
+    if (v.IsEmpty()) {
+      Handle<Value> exception = trycatch.Exception();
 
-    while (*dst++ = *src++) {
-      // Nada!
+      wchar_t *result = fromValue(exception);
+
+      // magic vs more magic
+      context.Dispose();
+      errno = -69;
+
+      return result;
+      
+    } else {
+      wchar_t *result = fromValue(v);
+
+      context.Dispose();
+      errno = 0;
+      
+      return result;
     }
-
-    context.Dispose();
-
-    return result;
   }
 
   return NULL;
