e0e45291de85c4debd0a0b42aee99e50955d936e
Paul Biggar <paul.biggar@gmail.com>
2012-12-16 14:33:59 -0800

    Tidy up formatting and comments

--- clj-v8/project.clj
+++ clj-v8/project.clj
@@ -2,9 +2,8 @@
   :description "A Clojure wrapper for the v8 Javascript library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.elmom/clojure-jna "0.9"]
- 		 [net.java.dev.jna/jna "3.5.1"]
+                 [net.java.dev.jna/jna "3.5.1"]
                  [clj-v8-native/clj-v8-native "0.1.3"]]
   :dev-dependencies [[midje "1.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]]}}
-  :jvm-opts ["-Djna.library.path=target/native/macosx/x86_64:target/native/linux/x86_64:target/native/linux/x86:native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"
-             "-Djava.library.path=target/native/macosx/x86_64:target/native/linux/x86_64:target/native/linux/x86:native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"])
+  :jvm-opts ["-Djava.library.path=target/native/macosx/x86_64:target/native/linux/x86_64:target/native/linux/x86:native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"])
--- clj-v8/src/v8/core.clj
+++ clj-v8/src/v8/core.clj
@@ -1,23 +1,30 @@
 ;; See https://github.com/overtone/overtone/blob/master/src/overtone/jna_path.clj
 ;; This should let us read straight from jars
 (defonce __SET_JNA_PATH__
-  (do
-  (println (System/getProperty "jna.library.path"))
-  (println (System/getProperty "java.library.path"))
-  (System/setProperty "jna.library.path" (System/getProperty "java.library.path"))))
+  (System/setProperty "jna.library.path" (System/getProperty "java.library.path")))
 
 (ns v8.core
   (:require [clojure.string :as str])
   (:import [com.sun.jna WString Native Memory Pointer NativeLibrary]))
 
-;; On linux, libraries are properly loaded using java.library.path/jna.library.path. However, that's only for libraries loaded directly from JNA. Libraries loaded by those libraries do not use java.library.path, only LD_LIBRARY_PATH, which is only set at the very start of the process and cannot be set in code. The solution is to load libv8.clj-v8 oursevles so that it is already loaded when v8wrapper is loaded.
-;; In addition, while JNA allows you to specify the full name of a lib (that is, containing the "lib" and the version), it only allows this when the version number is all digits and dots. We can work around it with an absolute path.
+;; On linux, libraries are properly loaded using
+;; java.library.path/jna.library.path. However, that's only for libraries loaded
+;; directly from JNA. Libraries loaded by those libraries do not use
+;; java.library.path, only LD_LIBRARY_PATH, which is only set at the very start
+;; of the process and cannot be set in code. The solution is to load
+;; libv8.clj-v8 oursevles so that it is already loaded when v8wrapper is loaded.
+;;
+;; In addition, while JNA allows you to specify the full name of a lib (that is,
+;; containing the "lib" and the version), it only allows this when the version
+;; number is all digits and dots. We can work around it with an absolute path.
 (when (com.sun.jna.Platform/isLinux)
   (doseq [path (str/split (System/getProperty "jna.library.path") #":")]
     (let [abs (-> (str path "/" "libv8.so.clj-v8") java.io.File. .getAbsolutePath)]
       (com.sun.jna.NativeLibrary/getInstance abs))))
 
 (def LIBRARY (com.sun.jna.NativeLibrary/getInstance "v8wrapper"))
+
+
 
 (def run-fn (.getFunction LIBRARY "run"))
 (def create-tuple-fn (.getFunction LIBRARY "create_tuple"))
