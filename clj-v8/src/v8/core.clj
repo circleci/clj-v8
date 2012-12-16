ed1222e54e0fe90e9aec813969bcc81ffae5b995
Paul Biggar <paul.biggar@gmail.com>
2012-12-16 00:23:59 -0800

    Remove unnecessary locking (v8 does it for us)

--- clj-v8/src/v8/core.clj
+++ clj-v8/src/v8/core.clj
@@ -8,22 +8,18 @@
 (def create-tuple-fn (.getFunction LIBRARY "create_tuple"))
 (def cleanup-tuple-fn (.getFunction LIBRARY "cleanup_tuple"))
 
-
-(def v8LockingObject (Object.))
-
 (defn run-script
   "Compiles and runs a JS file"
   [script]
-  (locking v8LockingObject
-    (let [tuple (.invokePointer create-tuple-fn (into-array []))
-          result (.invoke run-fn Memory (object-array [tuple (new WString script)]))
-          strresult (if (nil? result) nil (.getString result 0 true))]
+  (let [tuple (.invokePointer create-tuple-fn (into-array []))
+        result (.invoke run-fn Memory (object-array [tuple (new WString script)]))
+        strresult (if (nil? result) nil (.getString result 0 true))]
 
-      ;; result will be cleaned up by the finalizer, hopefully
-      (when (not= (. Native getLastError) 0)
-        (.invokeVoid cleanup-tuple-fn (into-array [tuple]))
-        (if (nil? result)
-          (throw (Exception. "V8 reported error, but message is null!"))
-          (throw (Exception. (str "V8 error: " strresult)))))
+    ;; result will be cleaned up by the finalizer, hopefully
+    (when (not= (. Native getLastError) 0)
       (.invokeVoid cleanup-tuple-fn (into-array [tuple]))
-      strresult)))
+      (if (nil? result)
+        (throw (Exception. "V8 reported error, but message is null!"))
+        (throw (Exception. (str "V8 error: " strresult)))))
+    (.invokeVoid cleanup-tuple-fn (into-array [tuple]))
+    strresult))
